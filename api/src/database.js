edeae169d1232f9d92bcf50c2041fd13f60e33d9
Michelle Diaz <michellelunaby@gmail.com>
2023-06-07 03:09:12 -0600

    modify back

--- api/src/database.js
+++ api/src/database.js
@@ -1,18 +1,18 @@
- import dotenv from 'dotenv';
- import pg from 'pg';
-import { Sequelize } from 'sequelize';
-dotenv.config();
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+//  import dotenv from 'dotenv';
+//  import pg from 'pg';
+// import { Sequelize } from 'sequelize';
+// dotenv.config();
+// const {DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
- const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectModule: pg,
-});
+//  const sequelize = new Sequelize(DB_DEPLOY, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   dialectModule: pg,
+// });
 
- // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
- //   logging: false, // set to console.log to see the raw SQL queries
- //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
- // });
+//  // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//  //   logging: false, // set to console.log to see the raw SQL queries
+//  //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//  // });
  
- export default sequelize;
+//  export default sequelize;
